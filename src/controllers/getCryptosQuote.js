199619ee4b65acc8761ee4a2681a68bbbc32ea1c
Dievano Dantas <dievano_dantas@hotmail.com>
2020-11-19 23:39:52 -0300

    Improved API, returning more results

--- src/controllers/getCryptosQuote.js
+++ src/controllers/getCryptosQuote.js
@@ -2,6 +2,7 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 const dotenv = require('dotenv');
 const validate = require('../helpers/validate');
+const cryptosLibrary = require('../helpers/cryptosLibrary');
 
 dotenv.config({ path: './src/config/.env' });
 
@@ -21,39 +22,41 @@ function loadData() {
         .then($ => {
             cryptos.length = 0;
 
-            // HERE IS THE PART WHERE THE NAME AND SYMBOL OF THE CRYPTOCURRENCIES ARE CAPTURED, AND THESE DATA ARE
+            // HERE IS THE PART WHERE THE NAME OF THE CRYPTOCURRENCIES ARE CAPTURED, AND THESE DATA ARE
             // STORED IN A TEMPORARY ARRAY
-            let arrData = [];
-            $('.Box-sc-16r8icm-0.CoinItem__NameArea-sc-1teo54s-1.NqdZD').each((i, item) => {
-                let data = {
-                    name: $(item).find('.Text-sc-1eb5slv-0.iTmTiC').text(),
-                    symbol: $(item).find('.Text-sc-1eb5slv-0.eweNDy.coin-item-symbol').text(),
-                };
-                arrData.push(data);
+            let arrNames = [];
+            $('table > tbody > tr > td:nth-child(1)').each((i, item) => {
+                let name = $(item).find('a.tv-screener__symbol').text();
+                if (validate.validateData(name)) {
+                    arrNames.push(name);
+                }
             });
-
-            // console.log(arrData.length);
 
             // HERE IS THE SECOND ARRAY THAT CAPTURES ONLY CRYPTOCURRENCIES PRICES
             let arrPrices = [];
             $('table > tbody > tr > td:nth-child(4)').each((i, item) => {
-                let price = $(item).find('a.cmc-link').text();
+                let price = $(item).text();
                 if (validate.validateData(price)) {
-                    price = price.replace('$', '').replace(',', '');
                     arrPrices.push(price);
                 }
             });
 
-            // console.log(arrPrices.length);
-
             // HERE A CHECK OF THE 2 AUXILIARY ARRAYS IS MADE, ASSEMBLING AN OBJECT AND ADDING IT
             // IN THE MAIN ARRAY
-            if (arrData.length === arrPrices.length) {
-                for (let i = 0; i < arrData.length; i++) {
+            if (arrNames.length === arrPrices.length) {
+                for (let i = 0; i < arrNames.length; i++) {
+                    let symbol_found;
+                    let obj = cryptosLibrary.CRYPTOS.find(crypto => crypto.name == arrNames[i]);
+                    if (obj) {
+                        symbol_found = obj.symbol;
+                    } else {
+                        symbol_found = 'undefined';
+                    }
+
                     let crypto = {
                         position: (i + 1),
-                        name: arrData[i].name,
-                        symbol: arrData[i].symbol,
+                        name: arrNames[i],
+                        symbol: symbol_found,
                         price: arrPrices[i]
                     }
                     cryptos.push(crypto);
